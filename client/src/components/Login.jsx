356315a34884607a4caf560e3fa4f148b8d3314f
priyanshuu-02 <priyanshu.23bce8218@vitapstudent.ac.in>
2025-07-31 14:07:39 +0530

    Fix: updated axios base URL and ensured consistent import

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+import axios from '../axios';
 import { Link, useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css';
